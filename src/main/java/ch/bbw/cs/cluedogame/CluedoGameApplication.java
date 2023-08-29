04ee955479bf2e02f85f06066992a06353df6ec4
cxs1mon <caroline.simon@gmx.ch>
2023-08-29 12:33:12 +0200

    initial commit

---
+++ src/main/java/ch/bbw/cs/cluedogame/CluedoGameApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.cs.cluedogame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CluedoGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CluedoGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/bbw/cs/cluedogame/CluedoGameApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.cs.cluedogame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CluedoGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
